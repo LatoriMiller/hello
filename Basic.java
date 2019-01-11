108a4cf0d2d224570c4aeb7518acf4754bfa81af
LatoriMiller <latori.miller@gmail.com>
2019-01-11 13:56:44 -0500

    commit from source for Jenkins

---
+++ Basic.java
@@ -0,0 +1,9 @@
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Latori I am building this project all on my own");
+	}
+
+}
